2e4648899709885af750c96919592f70fc749002
sumankhalia <sumankhalia05@gmail.com>
2026-02-18 21:34:19 +0530

    Aligned DB fallback to omni.db

--- database/db_engine.py
+++ database/db_engine.py
@@ -1,13 +1,16 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+# FALLBACK
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./omni.db"
 
-Base = declarative_base()
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
